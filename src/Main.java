1ce3009ce10db6925455e0daaf65fe2607ee5bb1
Serg <ksyssau@ya.ru>
2023-04-13 21:19:47 +0300

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,24 @@
 public class Main {
     public static void main(String[] args) {
 
+        task1();
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        int summ = 0;
+        int[] arr = generateRandomArray();
+        for (int i = 0; i < arr.length; i++) {
+            summ = summ + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + summ + " рублей.");
     }
 }
